ae2f2438f5b4360c334c05d648a37d41bd94aef1
nahir25 <nahirwanda2002@gmail.com>
2023-01-04 20:10:39 -0300

    cambie un numero

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = hola;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 8;
+const nuevoNum = 10;
 
 // Crea una variable booleana:
 const nuevoBool = true;
